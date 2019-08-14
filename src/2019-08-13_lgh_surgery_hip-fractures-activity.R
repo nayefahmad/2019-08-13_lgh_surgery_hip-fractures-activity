cfc3ff85b26dda3891c3f764c85238add1651c92
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2019-08-13 17:23:50 -0700

    list of surg_dx_desc and code - FAIL; can't find in denodo

--- src/2019-08-13_lgh_surgery_hip-fractures-activity.R
+++ src/2019-08-13_lgh_surgery_hip-fractures-activity.R
@@ -11,4 +11,47 @@
 #'     toc_float: true
 #' ---
 
+#' jira ds-3203
+#' goal: to update the LGH Hip Fracture tables (see attached) to FY 2019/20 YTD
+
+library(tidyverse)
+library(denodoExtractor)
+
+setup_denodo()
+
+
+# set up denodo surgery view: 
+vw_surgery_completed_cases <- dplyr::tbl(cnx, 
+                                         dbplyr::in_schema("publish", 
+                                                           "surgery_case_completed"))
+
+
+
+# dx codes for hip fractures: 
+df1.hip_fracture_dx <- 
+  data.frame(surg_dx_desc = 
+                c("Other fracture of femoral neck, closed",
+                  "Intertrochanteric fracture, closed",
+                  "Unspecified fracture of neck of femur, closed",
+                  "Subtrochanteric fracture, closed",
+                  "Unspecified trochanteric fracture, closed",
+                  "Fracture of base of femoral neck (cervicotrochanteric) closed",
+                  "Mechanical complication of hip prosthesis, breakage and dissociation",
+                  "Fracture of acetabulum, closed",
+                  "Fracture of femur, part unspecified, closed",
+                  "Intertrochanteric fracture, open", 
+                  "Screening Colonoscopy; Not BCCA pers/sig. family history")) %>%  # last one is just to test the join
+  
+  left_join(vw_surgery_completed_cases %>% 
+              select(surg_dx_desc, 
+                     surg_dx_cd) %>% 
+              distinct() %>% 
+              collect())
+
+
+
+
+
+
+
 
