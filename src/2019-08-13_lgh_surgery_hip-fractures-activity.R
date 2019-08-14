2f668fda8cf6b7312b0d70667d3a62ea19a7e017
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2019-08-13 17:02:20 -0700

    initial

---
+++ src/2019-08-13_lgh_surgery_hip-fractures-activity.R
@@ -0,0 +1,14 @@
+
+#'--- 
+#' title: "LGH - Hip fractures with surgical interventions "
+#' author: "Nayef Ahmad"
+#' date: "2019-08-13"
+#' output: 
+#'   html_document: 
+#'     keep_md: yes
+#'     code_folding: show
+#'     toc: true
+#'     toc_float: true
+#' ---
+
+
